abf618ff894021b61af96297e275f4d2e731a0d1
ajithkumark771 <35993027+ajithkumark771@users.noreply.github.com>
2020-06-17 13:15:57 +0530

    Add files via upload

---
+++ UITableView+.swift
@@ -0,0 +1,28 @@
+//
+//  UITableView+.swift
+//  JKShah-Online
+//
+//  Created by ajith on 17/06/20.
+//  Copyright © 2020 ajith. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITableView{
+    func updateEmptyState(rowsCount: Int, emptyMessage: String){
+        rowsCount == 0 ? showEmptyState(emptyMessage) : hideEmptyState()
+        self.reloadData()
+    }
+    
+    private func showEmptyState(_ message: String){
+        let label = UILabel(frame: .zero)
+        label.text = message
+        label.textAlignment = .center
+        backgroundView = label
+    }
+    
+    private func hideEmptyState(){
+        backgroundView = nil
+    }
+}
